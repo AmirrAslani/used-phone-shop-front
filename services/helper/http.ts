30eb7699282276c61a932a3de45b421d0a2025d1
AmirrAslani <amirmohammad9992@gmail.com>
2025-08-19 16:13:23 +0330

    fix ngrok

--- services/helper/http.ts
+++ services/helper/http.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // ساخت instance اختصاصی axios
 const http = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://5386a6effdf1.ngrok-free.app/api",
   headers: {
     "Content-Type": "application/json",
   },
