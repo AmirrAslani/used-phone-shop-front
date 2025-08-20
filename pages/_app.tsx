3ca6a182307a2720eb7c26a3b277973ec6e445c3
AmirrAslani <amirmohammad9992@gmail.com>
2025-08-20 05:33:48 +0330

    fix toastify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import { ToastContainer } from 'react-toastify';
 import type { AppProps } from "next/app";
 import Navbar from "@/layouts/navbar/Navbar";
 import { NextComponentType } from 'next';
-// import Animate from 'animate.css-react'
 import 'animate.css/animate.css'
 type PageWithLayout = {
   getLayout?: (page: React.ReactElement) => React.ReactNode;
@@ -18,7 +17,7 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => (
     <>
-      <ToastContainer rtl />
+      <ToastContainer rtl className="z-99"/>
       <Navbar />
       {page}
     </>
